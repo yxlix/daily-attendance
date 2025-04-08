9b6791cf686b789f920491ff51cd3abcf67c73d8
SinyLi <yxlix1@163.com>
2025-04-08 15:33:48 +0800

    测试

---
+++ sucai999.js
@@ -0,0 +1,18 @@
+/**
+ * 菜鸟图库签到
+ * cron 11 12 * * *  sucai999.js
+ */
+const name = '菜鸟图库';
+//取cookie
+let cookiesArr = [];
+let cookie = '';
+if (process.env.SMZDM_COOKIE) {
+    if (process.env.SMZDM_COOKIE.indexOf('&') > -1) {
+        cookiesArr = process.env.SMZDM_COOKIE.split('&');
+    } else if (process.env.SMZDM_COOKIE.indexOf('\n') > -1) {
+        cookiesArr = process.env.SMZDM_COOKIE.split('\n');
+    } else {
+        cookiesArr = [process.env.SMZDM_COOKIE];
+    }
+}
+QLAPI.systemNotify(name,'123');
